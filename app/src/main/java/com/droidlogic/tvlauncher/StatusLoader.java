158b038526da01ac465c6d91b75453c3ac6be3e4
Sergey Lagutkin <serlag@yandex.ru>
2024-04-05 21:13:18 +0300

    fix statusbar

--- app/src/main/java/com/droidlogic/tvlauncher/StatusLoader.java
+++ app/src/main/java/com/droidlogic/tvlauncher/StatusLoader.java
@@ -22,51 +22,83 @@ import java.util.Locale;
 
 public class StatusLoader {
     private final static String TAG = "StatusLoader";
-    private ConnectivityManager mConnectivityManager;
+    public static final String ICON ="item_icon";
+
     private Context mContext;
-    private StorageManager mStorageManager;
+    private ConnectivityManager mConnectivityManager;
     private WifiManager mWifiManager;
+    private StorageManager mStorageManager;
+    private int wifi_level;
+    private boolean isSdcardExist;
+    private boolean isUdiskExist;
+    private boolean isEthernetOn;
+
+    private List<ArrayMap<String, Object>> storage;
 
     public StatusLoader(Context context) {
         mContext = context;
         mConnectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         mStorageManager = (StorageManager) mContext.getSystemService(Context.STORAGE_SERVICE);
+        storage = new ArrayList<ArrayMap<String, Object>>();
     }
 
     public  List<ArrayMap<String, Object>> getStatusData() {
-        List<ArrayMap<String, Object>> list = new ArrayList<>();
-        ArrayMap<String, Object> map = new ArrayMap<>();
-        // Check network status
-        NetworkInfo info = mConnectivityManager.getActiveNetworkInfo();
-        if (info != null && info.getType() == ConnectivityManager.TYPE_ETHERNET) {
-            map.put("item_icon", R.drawable.img_status_ethernet);
-            list.add(map);
-        }
-        if (info != null && info.getType() == ConnectivityManager.TYPE_WIFI) {
-            WifiInfo mWifiInfo = mWifiManager.getConnectionInfo();
-            int wifi_level = WifiManager.calculateSignalLevel(mWifiInfo.getRssi(), 4);
+        List<ArrayMap<String, Object>> list = new ArrayList<ArrayMap<String, Object>>();
+        ArrayMap<String, Object> map = new ArrayMap<String, Object>();
+        updateNetworkStatus();
+        updateStorageStatus();
+
+        if (wifi_level != -1) {
             switch (wifi_level + 1) {
                 case 1:
-                    map.put("item_icon", R.drawable.wifi2);
+                    map.put(ICON, R.drawable.wifi2);
                     break;
                 case 2:
-                    map.put("item_icon", R.drawable.wifi3);
+                    map.put(ICON, R.drawable.wifi3);
                     break;
                 case 3:
-                    map.put("item_icon", R.drawable.wifi4);
+                    map.put(ICON, R.drawable.wifi4);
                     break;
                 case 4:
-                    map.put("item_icon", R.drawable.wifi5);
+                    map.put(ICON, R.drawable.wifi5);
                     break;
                 default:
                     break;
             }
+            list.add(map);
         }
 
-        // Check removable drives
+        if (isSdcardExist) {
+            map = new ArrayMap<String, Object>();
+            map.put(ICON, R.drawable.img_status_sdcard);
+            list.add(map);
+        }
+
+        if (isUdiskExist) {
+            map = new ArrayMap<String, Object>();
+            map.put(ICON, R.drawable.img_status_usb);
+            list.add(map);
+        }
+
+        boolean is_ethernet_on = isEthernetOn;
+        if (is_ethernet_on == true) {
+            map = new ArrayMap<String, Object>();
+            map.put(ICON, R.drawable.img_status_ethernet);
+            list.add(map);
+        }
+
+        return list;
+    }
+
+    private void updateStorageStatus() {
         List<StorageVolume> volumeList =  mStorageManager.getStorageVolumes();
+        ArrayMap<String, Object> map = new ArrayMap<String, Object>();
         String mId = "";
+        isSdcardExist = false;
+        isUdiskExist = false;
+        storage.clear();
+
         for (StorageVolume volume : volumeList) {
             if (null != volume && volume.isRemovable()) {
                 try {
@@ -78,18 +110,29 @@ public class StatusLoader {
                     Log.e(TAG, e.getMessage());
                 }
                 if (mId.startsWith("public:179")) {
-                    map = new ArrayMap<String, Object>();
-                    map.put("item_icon", R.drawable.img_status_sdcard);
-                    list.add(map);
+                    isSdcardExist = true;
                 }
                 if (mId.startsWith("public:8")) {
-                    map = new ArrayMap<String, Object>();
-                    map.put("item_icon", R.drawable.img_status_usb);
-                    list.add(map);
+                    isUdiskExist = true;
                 }
+                map.put("title", volume.getDescription(mContext));
+                map.put("id", mId);
+                storage.add(map);
             }
         }
-        return list;
+    }
+
+    private void updateNetworkStatus() {
+        isEthernetOn = false;
+        wifi_level = -1;
+        NetworkInfo info = mConnectivityManager.getActiveNetworkInfo();
+        if (info != null && info.getType() == ConnectivityManager.TYPE_ETHERNET) {
+            isEthernetOn = true;
+        }
+        if (info != null && info.getType() == ConnectivityManager.TYPE_WIFI) {
+            WifiInfo mWifiInfo = mWifiManager.getConnectionInfo();
+            wifi_level = WifiManager.calculateSignalLevel(mWifiInfo.getRssi(), 4);
+        }
     }
 
     public  String getTime(){
@@ -129,10 +172,15 @@ public class StatusLoader {
         String date;
         if (Locale.getDefault().getLanguage().equals("zh")) {
             date = str_week + ", " + mMonth + " " + mDay + mContext.getResources().getString(R.string.str_day);
+        }else if (Locale.getDefault().getLanguage().equals("ru")) {
+            date = str_week + ", " + mDay + " " + mMonth;
         }else {
             date = str_week + ", " + mMonth + " " + mDay;
         }
-
         return date;
+    }
+
+    public List<ArrayMap<String, Object>> getStorage() {
+        return storage;
     }
 }
